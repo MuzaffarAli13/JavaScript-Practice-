d84b6f4461cae4ff563abc8cc8bcc4135589210f
Muzaffar Ali <muzafark834@gmail.com>
2025-09-07 23:25:41 +0500

    js practice

--- script.js
+++ script.js
@@ -96,15 +96,21 @@ for (const key in student_names) {
 
 
 // Initialize an array with color names. Display the array elements in your browser.
-
-// b. Ask the user what color he/she wants to add to the end & add that color to the end of the array. Display the updated array in your browser.
-// c. Add two more color to the beginning of the array. Display the updated array in your browser.
-// d. Delete the first color in the array. Display the updated array in your browser.
-
-// e. Delete the last color in the array. Display the updated array in your browser.
-// f. Ask the user at which index he/she wants to add a color & color name. Then add the color to desired position/index. . Display the updated array in your browser.
-// g. Ask the user at which index he/she wants to delete color(s) & how many colors he/she wants to delete. Then remove the same number of color(s) from user-defined position/index. . Display the updated array in your browser
 let color_names = ["Green","Red","Yellow"];
 console.log(color_names);
 // a. Ask the user what color he/she wants to add to the beginning & add that color to the beginning of the array. Display the updated array in your browser.
-color_names.push()
+let user = prompt("Enter Color Name:")
+color_names.push(user)
+
+// b. Ask the user what color he/she wants to add to the end & add that color to the end of the array. Display the updated array in your browser.
+
+// c. Add two more color to the beginning of the array. Display the updated array in your browser.
+
+// d. Delete the first color in the array. Display the updated array in your browser.
+
+// e. Delete the last color in the array. Display the updated array in your browser.
+
+// f. Ask the user at which index he/she wants to add a color & color name. Then add the color to desired position/index. . Display the updated array in your browser.
+
+// g. Ask the user at which index he/she wants to delete color(s) & how many colors he/she wants to delete. Then remove the same number of color(s) from user-defined position/index. . Display the updated array in your browser
+
