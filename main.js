9b554218930a45ee199e46c938df50270c6ce8f5
Muzaffar Ali <muzafark834@gmail.com>
2025-09-11 16:47:43 +0500

    js practice by sir zia

--- main.js
+++ main.js
@@ -224,5 +224,51 @@
 
 
 
+// // 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// let famous_person = "Ronaldo"
+// let message = "Hello Dear"
+// console.log(`${message} ${famous_person}`);
+
+
+
+// 6. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+// let person_name = "   Muzaffar Ali   ";
+// console.log(`with whitespace:\n${person_name}`);
+// console.log(`without whitespace:\n${person_name.trim()}`);
+
+
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results. 
+// let num = 2
+// console.log(num + 6);
+// console.log(10 - num);
+// console.log(num * 4);
+// console.log(16 / num);
+
+// 8. You should create four lines that look like this:
+
+// _____________________________________________
+
+// console.log(5 + 3)
+
+// _____________________________________________
+
+// console.log("_".repeat(20));
+// console.log(5 + 3);
+// console.log(10 - 2);
+// console.log(16 / 2);
+// console.log(2 * 4);
+// console.log("_".repeat(20));
+
+
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+// let favorite_num = 100;
+// console.log(`My favorite Number is ${favorite_num}`);
+
+// 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write
+// because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence
+// describing what the program does.
+// let current_date = new Date()
+// console.log(current_date.toDateString())
+// ;
 
 
