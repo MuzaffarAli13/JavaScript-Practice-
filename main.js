9cb388c82bef0988440373dd56f9bcf53b7319ce
Muzaffar Ali <kalwar12khan@gmail.com>
2025-10-04 23:51:09 +0500

    js daily practice logic building

--- main.js
+++ main.js
@@ -381,9 +381,38 @@
 
 // 45 assignments start again
 // 11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
-let names = ["babar","rizwan","Ali"]
-names.sort();
-for(let i=0;i < names.length;i++){
-     console.log(names[i]);     
+// let names = ["babar","rizwan","Ali"]
+// names.sort();
+// for(let i=0;i < names.length;i++){
+//      console.log(names[i]);     
+// }
+
+// 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message
+// should be the same, but each message should be personalized with the person’s name.
+// let names = ["babar", "rizwan", "Ali"];
+
+// for (let i = 0; i < names.length; i++) {
+//   console.log(`Hello ${names[i]}, you are my best friend!`);
+// }
+
+
+
+// 13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+// to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+// let favorite_transportation = ["Honda Car","BRV","GLI"]
+// favorite_transportation.map(item =>{
+//      console.log(`I would like to own a ${item}.`);
+// })
+
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+// invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let guest_list = ["Aleeza","Ghazala","Daraiba"]
+
+for(let i=0;i < guest_list.length;i++){
+     console.log(`Dear ${guest_list[i]}, you are invited to dinner!`);    
 }
+
+
+
 
